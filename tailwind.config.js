bd203f8c47fdee15df566aaf799dd52503003895
Ali <120646614+alizamanian79@users.noreply.github.com>
2025-01-22 13:04:14 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,74 @@
 
-module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", 
-  ],
-  theme: {
-  	extend: {
-  		fontFamily: {
-  			iranyekan: ['IRANYekan', 'sans-serif']
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-
-  screens: {
-	'sm': {'min': '640px', 'max': '767px'},
+export const darkMode = ["class"];
+export const content = [
+	"./index.html",
+	"./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+	extend: {
+		fontFamily: {
+			iranyekan: ['IRANYekan', 'sans-serif']
+		},
+		borderRadius: {
+			lg: 'var(--radius)',
+			md: 'calc(var(--radius) - 2px)',
+			sm: 'calc(var(--radius) - 4px)'
+		},
+		colors: {
+			background: 'hsl(var(--background))',
+			foreground: 'hsl(var(--foreground))',
+			card: {
+				DEFAULT: 'hsl(var(--card))',
+				foreground: 'hsl(var(--card-foreground))'
+			},
+			popover: {
+				DEFAULT: 'hsl(var(--popover))',
+				foreground: 'hsl(var(--popover-foreground))'
+			},
+			primary: {
+				DEFAULT: 'hsl(var(--primary))',
+				foreground: 'hsl(var(--primary-foreground))'
+			},
+			secondary: {
+				DEFAULT: 'hsl(var(--secondary))',
+				foreground: 'hsl(var(--secondary-foreground))'
+			},
+			muted: {
+				DEFAULT: 'hsl(var(--muted))',
+				foreground: 'hsl(var(--muted-foreground))'
+			},
+			accent: {
+				DEFAULT: 'hsl(var(--accent))',
+				foreground: 'hsl(var(--accent-foreground))'
+			},
+			destructive: {
+				DEFAULT: 'hsl(var(--destructive))',
+				foreground: 'hsl(var(--destructive-foreground))'
+			},
+			border: 'hsl(var(--border))',
+			input: 'hsl(var(--input))',
+			ring: 'hsl(var(--ring))',
+			chart: {
+				'1': 'hsl(var(--chart-1))',
+				'2': 'hsl(var(--chart-2))',
+				'3': 'hsl(var(--chart-3))',
+				'4': 'hsl(var(--chart-4))',
+				'5': 'hsl(var(--chart-5))'
+			}
+		}
+	}
+};
+export const plugins = ["tailwindcss-animate"];
+export const screens = {
+	'sm': { 'min': '640px', 'max': '767px' },
 	// => @media (min-width: 640px and max-width: 767px) { ... }
-
-	'md': {'min': '768px', 'max': '1023px'},
+	'md': { 'min': '768px', 'max': '1023px' },
 	// => @media (min-width: 768px and max-width: 1023px) { ... }
-
-	'lg': {'min': '1024px', 'max': '1279px'},
+	'lg': { 'min': '1024px', 'max': '1279px' },
 	// => @media (min-width: 1024px and max-width: 1279px) { ... }
-
-	'xl': {'min': '1280px', 'max': '1535px'},
+	'xl': { 'min': '1280px', 'max': '1535px' },
 	// => @media (min-width: 1280px and max-width: 1535px) { ... }
-
-	'2xl': {'min': '1536px'},
+	'2xl': { 'min': '1536px' },
 	// => @media (min-width: 1536px) { ... }
-  },
 };
 
